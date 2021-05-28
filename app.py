c8f2f6009f9b0ed16269d8d0bc64d28dc8f6408d
njneubauer <njneubauer@gmail.com>
2021-05-27 21:06:06 -0500

    updated data validation code

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def calc_death_probability(clinical_data):
     model = joblib.load("complex_model.sav")
     return model.predict_proba(clinical_data)
 
-
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -31,19 +30,20 @@ def resources():
 
 @app.route('/clinical_data', methods=['GET','POST'])
 def clinical_data():
-    age = request.form['age']
-    gender = request.form['gender']
-    hypertension = request.form['hypertension']
-    smoker = request.form['smoker']
-    diabetes = request.form['diabetes']
-    anemia = request.form['anemia']
-    creatine = request.form['creatine_phosphate']
-    ejection_fraction = request.form['ejection_fraction']
-    platelets = request.form['platelets']
-    sodium = request.form['sodium']
-    office_vist = request.form['office_visit']
-
-    clinical_data_dict = {'age':age, 'anemia':anemia, 'creatine_phosphokinase':creatine, 'diabetes':diabetes, 'ejection_fraction': ejection_fraction,'high_blood_pressure': hypertension, 
+    age = request.form.get('age')
+    gender = request.form.get('gender')
+    hypertension = request.form.get('hypertension')
+    smoker = request.form.get('smoker')
+    diabetes = request.form.get('diabetes')
+    anemia = request.form.get('anemia')
+    creatine = request.form.get('creatine_phosphate')
+    ejection_fraction = request.form.get('ejection_fraction')
+    platelets = request.form.get('platelets')
+    sodium = request.form.get('sodium')
+    office_vist = request.form.get('office_visit')
+    
+    try:
+        clinical_data_dict = {'age':age, 'anemia':anemia, 'creatine_phosphokinase':creatine, 'diabetes':diabetes, 'ejection_fraction': ejection_fraction,'high_blood_pressure': hypertension, 
                           'platelets':platelets, 'serum_creatinine':creatine, 'serum_sodium':sodium, 'sex':gender, 'smoking':smoker, 'time':office_vist}
     
     clinical_data = pd.DataFrame(clinical_data_dict, index=[0])
@@ -54,7 +54,7 @@ def clinical_data():
 
     print(f'Result: {prob_clinical[0][0]}')
 
-    return render_template('clinical_calc.html', clinical_data=clinical_data, prob_clinical = prob_clinical)
+    return render_template('clinical_calc.html', clinical_data=clinical_data, prob_clinical = prob_clinical[0][0])
 
 @app.route('/patient_data', methods=['GET','POST'])
 def search():
@@ -76,7 +76,7 @@ def search():
     print(f'Result: {prob[0][0]}')
     
     #Plug  Machine learning values to send to a results.html screen
-    return render_template('patient_calc.html', prob=prob)
+    return render_template('patient_calc.html', prob=prob[0][0])
 
 if __name__ == "__main__":
     app.run(debug=True)
